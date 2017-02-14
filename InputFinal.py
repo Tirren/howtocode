2da7d025985c7fe99077fab7df783cbb82835a81
win7-df <ukr.lawliet@gmail.com>
2017-02-14 15:26:04 +0200

    Задание 2.

--- InputFinal.py
+++ InputFinal.py
@@ -10,20 +10,17 @@ def itsfloat(value):
 Num1 = input('Enter the first number ')
 
 if itsfloat(Num1):
+
     Num2 = input('Enter the second number ')
 
     if itsfloat(Num2):
-
         if float(Num1) > float(Num2):
             print(Num1, 'is the biggest number')
             print('Summ:', float(Num1)+float(Num2))
-
         else:
             print(Num2, 'is the biggest nubmer')
             print('Summ:', float(Num1)+float(Num2))
-
     else:
         print(Num2+str(pi))
-
 else:
         print(Num1+str(pi))
