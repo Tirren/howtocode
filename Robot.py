2cc4f94a7b9687c3604fcca84c968b66e39c8d7f
win7-df <ukr.lawliet@gmail.com>
2017-02-18 14:18:12 +0200

    Имена через функции.

--- Robot.py
+++ Robot.py
@@ -2,17 +2,17 @@ def Name(x):
     x = input('Enter the name ')
     return x
 
-N = Name(1)
+N = Name(True)
 
 def Count(y):
     return len(str(N))
 
 def OddEven(z):
-    if Count(2) % 2 == 0:
+    if Count(True) % 2 == 0:
         return 'It is an even number'
     else: return 'It is an odd number'
 
 def PrintRes(i):
-    print('Hello ' + N + '!', 'You have ' + str(Count(1)) + ' characters in your name.', OddEven(1), sep='\n')
+    print('Hello ' + N + '!', 'You have ' + str(Count(True)) + ' characters in your name.', OddEven(True), sep='\n')
 
-PrintRes(1)
+PrintRes(True)
