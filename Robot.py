b06ae6dee077035681ac68b44e349d03c9040a0f
win7-df <ukr.lawliet@gmail.com>
2017-02-28 17:52:15 +0200

    Робот с модулем.

--- Robot.py
+++ Robot.py
@@ -1,18 +1,17 @@
+import numc
+
 def Name():
     return input('Enter the name ')
 
-def Count(x):
-    return len(str(x))
-
 def OddEven(y):
-    if Count(y) % 2 == 0:
+    if numc.Count(y) % 2 == 0:
         return 'It is an even number'
     else:
         return 'It is an odd number'
 
 def PrintNames():
     a = Name()
-    b = Count(a)
+    b = numc.Count(a)
     c = OddEven(b)
     return print('Hello ' + a + '!', 'You have ' + str(b) + ' characters in your name.', c, sep='\n')
 
